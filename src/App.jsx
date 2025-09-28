aa16ff81e02c13ab56987e311ed531f72992612e
chandan-xcoder <chaandanverma@gmail.com>
2025-09-28 23:26:25 +0530

    keep

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Contact from './components/contact'
 import Skill from './components/Skill'
 import Project from './components/Project'
 import Footer from './components/Footer'
-import profileImage from "./assets/chandan.jpg";
 import './App.css'
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
   return (
     <>
      <Navbar />
-     <profileImage />
      <Hero />
      <AboutMe />
      <Skill />

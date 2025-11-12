db69ca3f301294f882c3865644f7a54be6164583
chandan-xcoder <chaandanverma@gmail.com>
2025-11-12 18:08:23 +0530

    feature

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
         </motion.h2>
 
         {/* Contact Form */}
-        <motion.div fk
+        <motion.div 
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.7 }}
